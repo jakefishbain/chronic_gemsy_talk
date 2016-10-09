eb7f54860edc289a0cb747e34f4143cc36e441f7
Jake Fishbain <jakefishbain@gmail.com>
2016-10-09 14:15:00 -0500

    toast sampler

--- app/assets/javascripts/chronic.js
+++ app/assets/javascripts/chronic.js
@@ -2,4 +2,10 @@
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+  
+
+    // Materialize.toast(message, displayLength, className, completeCallback);
+  Materialize.toast('', 4000) // 4000 is the duration of the toast
+
+
   });
--- app/models/chron.rb
+++ app/models/chron.rb
@@ -3,12 +3,18 @@ require 'chronic'
 class Chron < ActiveRecord::Base
   validates_presence_of :time
 
+  @options = ['thursday', 'november', 'summer', 'friday 13:00', 'mon 2:35', '4pm', '10 to 8', '10 past 2', 'half past 2', '6 in the morning', 'friday 1pm', 'sat 7 in the evening', 'yesterday', 'today', 'tomorrow', 'last week', 'next week', 'this tuesday', 'next month', 'last winter', 'this morning', 'last night', 'this second', 'yesterday at 4:00', 'last friday at 20:00', 'last week tuesday', 'tomorrow at 6:45pm', 'afternoon yesterday', 'thursday last week', '3 years ago', 'a year ago', '5 months before now', '7 hours ago', '7 days from now', '1 week hence', 'in 3 hours', '1 year ago tomorrow', '3 months ago saturday at 5:00 pm', '7 hours before tomorrow at noon', '3rd wednesday in november', '3rd month next year', '3rd thursday this september', '4th day last week', 'fourteenth of june 2010 at eleven oclock in the evening', 'may seventh 97 at three in the morning']
+
   def chronic_time
     self.time
   end
 
   def chronic_time=(time)
     self.time = Chronic.parse(time) if time
+  end
+
+  def self.option 
+    return @options.sample
   end
 
 end
